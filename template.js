18aaa4b960bb642f3d06f20cbf6e7b451ed42edd
mahbub14 <mahbubhasan14@gmail.com>
2020-04-15 06:40:58 +0600

    js practice

---
+++ template.js
@@ -0,0 +1,15 @@
+/* const firstName = "Rahim";
+ const lastName = "Uddin";
+ const fullName = firstName +" "+ lastName + "is a good boy.";
+console.log(fullName);
+
+const firstName = "Rahim";
+const lastName = "Uddin";
+const fullName2 = `${firstName} ${20+50+30} is a good boy`;
+console.log(fullName2);*/
+
+const multiline2 =`I love you.
+i miss you.
+no,I don't need you.`
+console.log(multiline2);
+
---
+++ threedots.js
@@ -0,0 +1,9 @@
+const ages = [12, 14, 16, 13, 17];
+const ages2 = [15, 16, 12];
+const ages3 = [25, 36, 22];
+const allAges = [...ages, ...ages2, 5, ...ages3]
+//console.log(allAges);
+//maximum number
+const takaPoisa = [650, 345, 908, 756];
+const maximum = Math.max(...takaPoisa);
+console.log(maximum);
